bce00dbd58db6c8cd3aeafdfcd6681f4a16a257e
InfiniteMagazine228 <haduykhanhpham@gmail.com>
2026-05-31 22:25:25 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,9 +2,11 @@
 set -e
 
 echo "Starting kernel build..."
+cd kernel   # vào thư mục chứa source kernel
+
 make O=out ARCH=arm64 defconfig
 make O=out ARCH=arm64 -j$(nproc)
 
-mkdir -p output
-cp out/arch/arm64/boot/Image.gz-dtb output/
+mkdir -p ../output
+cp out/arch/arm64/boot/Image.gz-dtb ../output/
 echo "Build completed!"
